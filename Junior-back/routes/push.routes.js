6ccafcd01fafaf0d98227ef70b964e3cfe5d74ac
YahirMtz12 <21161600@itoaxaca.edu.mx>
2026-08-15 13:08:17 -0600

    Fix middleware auth import path in push.routes.js

--- Junior-back/routes/push.routes.js
+++ Junior-back/routes/push.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth');
+const auth = require('../middleware/auth.middleware');
 const PushSubscription = require('../models/PushSubscription');
 const vapidKeys = require('../config/vapid');
 
